4f20ab5d531873932e9b2e7a2504a30b9081b777
XVs32 <makaut1995.cs02@g2.nctu.edu.tw>
2020-07-14 01:36:25 +0800

    press 'r' to stack item and sort inventory

--- src/main/java/com/technovision/chestmonster/events/InventoryGrappler.java
+++ src/main/java/com/technovision/chestmonster/events/InventoryGrappler.java
@@ -6,10 +6,13 @@ import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import static com.google.common.primitives.Ints.max;
+import static com.google.common.primitives.Ints.min;
 import static com.technovision.chestmonster.events.KeyboardEventHandler.keystate;
 
 public class InventoryGrappler {
@@ -19,35 +22,64 @@ public class InventoryGrappler {
     public static void onPlayerTick(TickEvent.PlayerTickEvent event){
 
         if(keystate == 1){
-            List<ItemStack> item;
-            item = event.player.inventory.mainInventory;
 
+            for(int i=9;i<36;i++){
 
-            System.out.println(item.size());
+                int j=9;
+                while( !event.player.inventory.mainInventory.get(i).isEmpty() && event.player.inventory.mainInventory.get(i).isStackable() ){
 
-            /*for(int i=0;i<item.size();i++){
-                //System.out.println(item.get(i).getDisplayName());
-                System.out.println("LOLL" + item.get(i).getItem().getTranslationKey());
+                    for(;j<i;j++){
+                        if( isSameItem(event,i,j) ){
+                            inventoryItemTransfer(event, i, j, 64);
+                            break;
+                        }
+                    }
 
-            }*/
+                    if(j == i){
+                        break;
+                    }
 
-            //event.player.inventory.mainInventory.set(2,item.get(0));
-            event.player.inventory.mainInventory.sort(new StringComparator());
+                }
+            }
 
-            System.out.println("LOLL" + item.get(0).getTranslationKey());
-            System.out.println("LOLL" + item.get(0).getCount());
-            System.out.println("LOLL" + item.get(0).getMaxStackSize());
+            event.player.inventory.mainInventory.subList(9,36).sort(new StringComparator());
+            //printInventory(event);
 
             keystate = 2;
         }
 
     }
 
+    private static boolean isSameItem(TickEvent.PlayerTickEvent event,int o1, int o2){
+        return event.player.inventory.mainInventory.get(o1).getTranslationKey().
+                equals(event.player.inventory.mainInventory.get(o2).getTranslationKey());
+    }
+
+    private static void inventoryItemTransfer(TickEvent.PlayerTickEvent event, int o1, int o2, int amount){
+        amount = Math.min(amount , event.player.inventory.mainInventory.get(o1).getMaxStackSize()
+                - event.player.inventory.mainInventory.get(o1).getCount());
+
+        amount = Math.min(amount , event.player.inventory.mainInventory.get(o2).getCount());
+
+        event.player.inventory.mainInventory.get(o1)
+                .setCount(event.player.inventory.mainInventory.get(o1).getCount() + amount);
+
+        event.player.inventory.mainInventory.get(o2)
+                .setCount(event.player.inventory.mainInventory.get(o2).getCount() - amount);
+    }
+
+    private static void printInventory(TickEvent.PlayerTickEvent event){
+       for(int i=0;i<36;i++){
+            System.out.println(event.player.inventory.mainInventory.get(i).getDisplayName() + " " + event.player.inventory.mainInventory.get(i).isStackable() + " " + event.player.inventory.mainInventory.get(i).getCount() + " " + event.player.inventory.mainInventory.get(i).getMaxStackSize() + " "  + event.player.inventory.mainInventory.get(i).isEmpty() );
+        }
+    }
+
+
     static class StringComparator implements Comparator {
 
         @Override
         public int compare(Object o1, Object o2) {
-            return ((ItemStack)o1).getTranslationKey().compareTo(((ItemStack)o2).getTranslationKey());
+            return -((ItemStack)o1).getTranslationKey().compareTo(((ItemStack)o2).getTranslationKey());
         }
     }
 
