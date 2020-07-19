d1a64a5b9d1de88a52b1906d18725d467a5c69ae
XVs32 <makaut1995.cs02@g2.nctu.edu.tw>
2020-07-19 23:15:46 +0800

    fix Inventory stacking, build the basic logic of chest and inventory data accessing

--- src/main/java/com/technovision/chestmonster/events/InventoryGrappler.java
+++ src/main/java/com/technovision/chestmonster/events/InventoryGrappler.java
@@ -11,6 +11,7 @@ import net.minecraft.world.IWorld;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.ChunkEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -32,65 +33,48 @@ import static java.lang.Float.valueOf;
 public class InventoryGrappler {
 
     private static ReentrantLock mutex = new ReentrantLock();
-    private static IWorld world;
+    private static ReentrantLock poMutex = new ReentrantLock();
+    private static BlockPos lastInteractPosition;
+    private static boolean chestFlag = false;
 
-    //@SubscribeEvent
+    @SubscribeEvent
     public static void onPlayerTick(TickEvent.PlayerTickEvent event){
-
 
         if(sorting == false){
             sorting = true;
-            BlockPos po = new BlockPos(0,80,0);
-            System.out.println("before");
-            TileEntity chestContainer = world.getTileEntity(po);
-            System.out.println("getTileEntity");
-            if(chestContainer != null){
-                System.out.println("not null");
 
-                LazyOptional<IItemHandler> itemHandler = chestContainer.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY);
-
-                event.player.
-                ItemStack tem = event.player.inventory.mainInventory.get(0);
-                System.out.println(tem.getDisplayName());
-
-                itemHandler.ifPresent(h -> System.out.println(h.isItemValid(5,tem)));
-
-                //itemHandler.ifPresent(h -> h.insertItem(5, new ItemStack(Items.IRON_INGOT) , false));
-                itemHandler.ifPresent(h -> System.out.println(h.getStackInSlot(5).getDisplayName()));
-
-
-
-                //itemHandler.insertItem(0,event.player.inventory.mainInventory.get(0),false);
-                //System.out.println(itemHandler.getStackInSlot(0).getDisplayName());
-            }
-
-
-
-/*
             mutex.lock();
 
             printChest(event);
 
             switch (event.player.openContainer.getInventory().size()){
                 case 46:
+                case 47:
                     System.out.println("Inventory");
-                    //event.player.inventory.mainInventory.get(0).setCount(5);
+
+                    for(int i=9;i<36;i++){
+                        int j=9;
+                        if( !event.player.inventory.mainInventory.get(i).isEmpty() && event.player.inventory.mainInventory.get(i).isStackable() ){
+                            for(;j<i;j++){
+                                if( isSameItemInPlayerTickEvent(event,i,j) ){
+                                    inventoryItemTransfer(event, i, j, 64);
+                                }
+                            }
+                        }
+                    }
+
                     event.player.inventory.mainInventory.subList(9,36).sort(new inventoryComparator());
+                    System.out.println("Inventory finish");
                     break;
                 case 63:
                     System.out.println("small chest");
-                    //event.player.openContainer.inventorySlots.get(0).getStack().setCount(5);
-
-                    //event.player.openContainer.getInventory().get(0).setCount(5);
-                    //event.player.inventory.mainInventory.subList(9,36).sort(new StringComparator());
-                    event.player.openContainer.inventorySlots.subList(0,27).sort(new containerComparator());
+                    chestFlag = true;
 
                     break;
                 case 90:
                     System.out.println("big chest");
-                    //event.player.inventory.mainInventory.get(0).setCount(5);
-                    //event.player.inventory.mainInventory.subList(9,36).sort(new StringComparator());
-                    event.player.openContainer.getInventory().subList(0,54).sort(new containerComparator());
+                    chestFlag = true;
+
                     break;
                 default:
                     System.out.println("mod? chest");
@@ -102,73 +86,100 @@ public class InventoryGrappler {
             mutex.unlock();
 
 
-
-            for(int i=9;i<36;i++){
-
-                int j=9;
-                while( !event.player.inventory.mainInventory.get(i).isEmpty() && event.player.inventory.mainInventory.get(i).isStackable() ){
-
-                    for(;j<i;j++){
-                        if( isSameItem(event,i,j) ){
-                            inventoryItemTransfer(event, i, j, 64);
-                            break;
-                        }
-                    }
-
-                    if(j == i){
-                        break;
-                    }
-
-                }
-            }
-
             //event.player.openContainer.getInventory()
             //printInventory(event);
 
-*/
+
         }
     }
 
     @SubscribeEvent
-    public static void onWorldLoad(TickEvent.WorldTickEvent event){
-        if(sorting == false){
-            sorting = true;
+    public static void onWorldTick(TickEvent.WorldTickEvent event){
 
-            System.out.println("onWorldLoad");
-            world = event.world;
-
-            BlockPos po = new BlockPos(0,80,0);
-            System.out.println("before");
-            TileEntity chestContainer = world.getTileEntity(po);
-
-            System.out.println("getTileEntity" + chestContainer);
-
+        if(chestFlag == true){
+            chestFlag = false;
+            BlockPos po = lastInteractPosition;
+            TileEntity chestContainer = event.world.getTileEntity(po);
+            System.out.println("getTileEntity");
             if(chestContainer != null){
                 System.out.println("not null");
 
                 LazyOptional<IItemHandler> itemHandler = chestContainer.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY);
 
-                itemHandler.ifPresent(h -> System.out.println(h.isItemValid(5,new ItemStack(Items.IRON_INGOT))));
+                itemHandler.ifPresent( h -> {
 
-                itemHandler.ifPresent(h -> System.out.println(h.getStackInSlot(5).getDisplayName()));
+                    int slotsCount = h.getSlots();
 
-                itemHandler.ifPresent(h -> h.insertItem(5, new ItemStack(Items.IRON_INGOT) , false));
+                    //todo: read slots into a List<itemstack>
 
+                    //todo: sort and stack the List<itemstack>
+
+                    //todo: write back the List<itemstack>
+
+                   /* for(int i=0;i<slotsCount;i++){
+                        ;
+                        if( !h.getStackInSlot(i).isEmpty() && h.getStackInSlot(i).isStackable() ){
+                            for(int j = slotsCount;j>i;j--){
+                                if( isSameItemInWorldTickEvent(h,i,j) ){
+                                    inventoryItemTransfer(event, i, j, 64);
+                                }
+                            }
+                        }
+                    }
+
+                    h..subList(9,36).sort(new inventoryComparator());
+
+                    System.out.println(slotsCount);
+
+                    for(int i=0;i< slotsCount;i++){
+                        System.out.println(h.getStackInSlot(i).getDisplayName());
+                    }
+
+                    h.insertItem(5, new ItemStack(Items.IRON_INGOT) , false);*/
+
+                });
+
+                //itemHandler.ifPresent(h -> h.insertItem(5, new ItemStack(Items.IRON_INGOT) , false));
+                //itemHandler.ifPresent(h -> System.out.println(h.getStackInSlot(5).getDisplayName()));
+
+                //itemHandler.insertItem(0,event.player.inventory.mainInventory.get(0),false);
+                //System.out.println(itemHandler.getStackInSlot(0).getDisplayName());
             }
-
         }
+    }
 
-
-
+    @SubscribeEvent
+    public static void onRightClick(PlayerInteractEvent.RightClickBlock event){
+        poMutex.lock();
+        lastInteractPosition = event.getPos();
+        poMutex.unlock();
     }
 
 
-    private static boolean isSameItem(TickEvent.PlayerTickEvent event, int o1, int o2){
+    private static boolean isSameItemInPlayerTickEvent(TickEvent.PlayerTickEvent event, int o1, int o2){
         return event.player.inventory.mainInventory.get(o1).getTranslationKey().
                 equals(event.player.inventory.mainInventory.get(o2).getTranslationKey());
     }
 
+    private static boolean isSameItemInWorldTickEvent(IItemHandler h, int o1, int o2){
+        return h.getStackInSlot(o1).getTranslationKey().
+                equals(h.getStackInSlot(o2).getTranslationKey());
+    }
+
     private static void inventoryItemTransfer(TickEvent.PlayerTickEvent event, int o1, int o2, int amount){
+        amount = Math.min(amount , event.player.inventory.mainInventory.get(o1).getMaxStackSize()
+                - event.player.inventory.mainInventory.get(o1).getCount());
+
+        amount = Math.min(amount , event.player.inventory.mainInventory.get(o2).getCount());
+
+        event.player.inventory.mainInventory.get(o1)
+                .setCount(event.player.inventory.mainInventory.get(o1).getCount() + amount);
+
+        event.player.inventory.mainInventory.get(o2)
+                .setCount(event.player.inventory.mainInventory.get(o2).getCount() - amount);
+    }
+
+    private static void chestItemTransfer(TickEvent.PlayerTickEvent event, int o1, int o2, int amount){
         amount = Math.min(amount , event.player.inventory.mainInventory.get(o1).getMaxStackSize()
                 - event.player.inventory.mainInventory.get(o1).getCount());
 
@@ -185,7 +196,7 @@ public class InventoryGrappler {
 
         System.out.println("size:" + event.player.openContainer.inventorySlots.size());
         for(int i=0;i<event.player.openContainer.inventorySlots.size();i++){
-            System.out.println(String.valueOf(i) + " " + event.player.openContainer.inventorySlots.get(i).getStack().getDisplayName() + " " + event.player.openContainer.inventorySlots.get(i).getStack().isStackable() + " " + event.player.openContainer.inventorySlots.get(i).getStack().getCount() + " " + event.player.openContainer.inventorySlots.get(i).getStack().getMaxStackSize() + " "  + event.player.openContainer.inventorySlots.get(i).getStack().isEmpty() );
+            System.out.println(i + " " + event.player.openContainer.inventorySlots.get(i).getStack().getDisplayName() + " " + event.player.openContainer.inventorySlots.get(i).getStack().isStackable() + " " + event.player.openContainer.inventorySlots.get(i).getStack().getCount() + " " + event.player.openContainer.inventorySlots.get(i).getStack().getMaxStackSize() + " "  + event.player.openContainer.inventorySlots.get(i).getStack().isEmpty() );
         }
     }
 
@@ -200,8 +211,8 @@ public class InventoryGrappler {
 
         @Override
         public int compare(Object o1, Object o2) {
-            String[] name1 = ((String) ((Slot) o1).getStack().getTranslationKey()).split("_|\\.");
-            String[] name2 = ((String) ((Slot) o2).getStack().getTranslationKey()).split("_|\\.");
+            String[] name1 = ((Slot) o1).getStack().getTranslationKey().split("_|\\.");
+            String[] name2 = ((Slot) o2).getStack().getTranslationKey().split("_|\\.");
 
             int i = name1.length - 1;
             int j = name2.length - 1;
